8ef53ac6045a531d7b49d8565c9e6d7e4be42f87
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-11-05 14:31:49 +0600

    file replace

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qu1uq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qu1uq.mongodb.net/online_shop?retryWrites=true&w=majority`
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-
+// console.log(object);
 async function run() {
     try {
         await client.connect();
+        console.log('mongodb connected');
         const database = client.db('online_shop');
         const productCollection = database.collection('products');
         const orderCollection = database.collection('orders');
@@ -67,8 +69,12 @@ async function run() {
 
 run().catch(console.dir);
 
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('Ema jon server is running');
+    res.send('Ema jon server is running..........');
 });
 
 app.listen(port, () => {
